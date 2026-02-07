a0589a694fb80075f70e4622c8d74f3940aa250f
Kik <rithishashwin02@gmail.com>
2026-02-07 12:15:03 -0500

    added ..\ to make it use the resources folder, that is a level up

--- scripts/auto_produce.py
+++ scripts/auto_produce.py
@@ -1,40 +1,40 @@
 from auto_login_wldm import *
 
 def auto_acryllic():
-    click_with_certainty(r"resources\images\obj_large_assembly_tool.png")
-    click_with_certainty(r"resources\images\button_use.png")
-    click_with_certainty(r"resources\images\list_elevator.png")
+    click_with_certainty(r"..\resources\images\obj_large_assembly_tool.png")
+    click_with_certainty(r"..\resources\images\button_use.png")
+    click_with_certainty(r"..\resources\images\list_elevator.png")
     for _ in range(5):
         pya.scroll(-2000)
     time.sleep(1)
-    click_with_certainty(r"resources\images\list_spacecraft.png")
-    click_with_certainty(r"resources\images\component_tit_ball.png")
-    click_with_certainty(r"resources\images\component_small_nuc_plant.png")
-    click_with_certainty(r"resources\images\component_refridgerant.png")
-    click_with_certainty(r"resources\images\component_water_pipe.png")
-    click_with_certainty(r"resources\images\component_acryllic_plate.png")
-    click_with_certainty(r"resources\images\button_ok.png")
-    click_with_certainty(r"resources\images\button_producting.png")
-    click_with_certainty(r"resources\images\button_start.png")
-    click_with_certainty(r"resources\images\list_sam.png")
+    click_with_certainty(r"..\resources\images\list_spacecraft.png")
+    click_with_certainty(r"..\resources\images\component_tit_ball.png")
+    click_with_certainty(r"..\resources\images\component_small_nuc_plant.png")
+    click_with_certainty(r"..\resources\images\component_refridgerant.png")
+    click_with_certainty(r"..\resources\images\component_water_pipe.png")
+    click_with_certainty(r"..\resources\images\component_acryllic_plate.png")
+    click_with_certainty(r"..\resources\images\button_ok.png")
+    click_with_certainty(r"..\resources\images\button_producting.png")
+    click_with_certainty(r"..\resources\images\button_start.png")
+    click_with_certainty(r"..\resources\images\list_sam.png")
     time.sleep(4*60)
 
 def auto_refridgerent():
-    click_with_certainty(r"resources\images\obj_large_assembly_tool.png")
-    click_with_certainty(r"resources\images\button_use.png")
-    click_with_certainty(r"resources\images\list_elevator.png")
+    click_with_certainty(r"..\resources\images\obj_large_assembly_tool.png")
+    click_with_certainty(r"..\resources\images\button_use.png")
+    click_with_certainty(r"..\resources\images\list_elevator.png")
     for _ in range(5):
         pya.scroll(-2000)
     time.sleep(1)
-    click_with_certainty(r"resources\images\list_spacecraft.png")
-    click_with_certainty(r"resources\images\component_tit_ball.png")
-    click_with_certainty(r"resources\images\component_small_nuc_plant.png")
-    click_with_certainty(r"resources\images\component_refridgerant.png")
+    click_with_certainty(r"..\resources\images\list_spacecraft.png")
+    click_with_certainty(r"..\resources\images\component_tit_ball.png")
+    click_with_certainty(r"..\resources\images\component_small_nuc_plant.png")
+    click_with_certainty(r"..\resources\images\component_refridgerant.png")
 
-    click_with_certainty(r"resources\images\button_ok.png")
-    click_with_certainty(r"resources\images\button_producting.png")
-    click_with_certainty(r"resources\images\button_start.png")
-    click_with_certainty(r"resources\images\list_sam.png")
+    click_with_certainty(r"..\resources\images\button_ok.png")
+    click_with_certainty(r"..\resources\images\button_producting.png")
+    click_with_certainty(r"..\resources\images\button_start.png")
+    click_with_certainty(r"..\resources\images\list_sam.png")
     time.sleep(12*60)
 
 def potion_diff_renew_timed(minutes, potion_pic):
@@ -45,41 +45,41 @@ def potion_diff_renew_timed(minutes, potion_pic):
     print("potion {%s} renewed.", potion_pic)
 
 def auto_propeller():
-    click_with_certainty(r"resources\images\obj_large_assembly_tool.png")
-    click_with_certainty(r"resources\images\button_use.png")
-    click_with_certainty(r"resources\images\list_elevator.png")
+    click_with_certainty(r"..\resources\images\obj_large_assembly_tool.png")
+    click_with_certainty(r"..\resources\images\button_use.png")
+    click_with_certainty(r"..\resources\images\list_elevator.png")
     for _ in range(5):
         pya.scroll(-2000)
     time.sleep(1)
-    click_with_certainty(r"resources\images\list_spacecraft.png")
-    click_with_certainty(r"resources\images\component_propeller.png")
-    click_with_certainty(r"resources\images\button_ok.png")
-    click_with_certainty(r"resources\images\button_producting.png")
-    click_with_certainty(r"resources\images\button_start.png")
-    click_with_certainty(r"resources\images\list_sam.png")
+    click_with_certainty(r"..\resources\images\list_spacecraft.png")
+    click_with_certainty(r"..\resources\images\component_propeller.png")
+    click_with_certainty(r"..\resources\images\button_ok.png")
+    click_with_certainty(r"..\resources\images\button_producting.png")
+    click_with_certainty(r"..\resources\images\button_start.png")
+    click_with_certainty(r"..\resources\images\list_sam.png")
     time.sleep(12*60)
 
 def auto_spacecraft():
-    click_with_certainty(r"resources\images\obj_large_assembly_tool.png")
-    click_with_certainty(r"resources\images\button_use.png")
-    click_with_certainty(r"resources\images\list_elevator.png")
+    click_with_certainty(r"..\resources\images\obj_large_assembly_tool.png")
+    click_with_certainty(r"..\resources\images\button_use.png")
+    click_with_certainty(r"..\resources\images\list_elevator.png")
     for _ in range(5):
         pya.scroll(-2000)
     time.sleep(1)
-    click_with_certainty(r"resources\images\list_spacecraft.png")
-    click_with_certainty(r"resources\images\button_ok.png")
-    click_with_certainty(r"resources\images\button_producting.png")
-    click_with_certainty(r"resources\images\button_start.png")
-    click_with_certainty(r"resources\images\list_sam.png")
+    click_with_certainty(r"..\resources\images\list_spacecraft.png")
+    click_with_certainty(r"..\resources\images\button_ok.png")
+    click_with_certainty(r"..\resources\images\button_producting.png")
+    click_with_certainty(r"..\resources\images\button_start.png")
+    click_with_certainty(r"..\resources\images\list_sam.png")
     time.sleep(60*60)
 
 def auto_france_church_bug():
-    click_with_certainty(r"resources\images\button_other_accounts_loading.png")
-    click_with_certainty(r"resources\images\button_login.png")
-    if click_with_certainty(r"resources\images\button_enter_game.png") is not None:
+    click_with_certainty(r"..\resources\images\button_other_accounts_loading.png")
+    click_with_certainty(r"..\resources\images\button_login.png")
+    if click_with_certainty(r"..\resources\images\button_enter_game.png") is not None:
         raise pya.ImageNotFoundException
     else:
-        click_with_certainty(r"resources\images\button_confirm.png")
+        click_with_certainty(r"..\resources\images\button_confirm.png")
 if __name__ == "__main__":
     time.sleep(10)
     ''' for _ in range(22):
@@ -93,7 +93,10 @@ if __name__ == "__main__":
     time.sleep(7*60)
     auto_propeller()
     auto_spacecraft()
-    '''
+     
     while True:
         auto_france_church_bug()
-
+        '''
+    time.sleep(102*60)
+    for _ in range(3):
+        click_with_certainty(r"..\resources\images\item_potion_2.png")
